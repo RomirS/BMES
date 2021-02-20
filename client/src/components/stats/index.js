e10aa0ef15aaa4c618e26d96d8eaf219f2285bf1
Aditya Dave <dave.aditya2017@gmail.com>
2021-02-19 18:07:53 -0600

    Added Stats page

--- client/src/components/stats/index.js
+++ client/src/components/stats/index.js
@@ -1,42 +1,95 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import Sidenav from '../home/Sidenav';
+import Loader from '../Loader';
 import './stats.css';
+
 
 class Stats extends Component {
     state = {
-        hours: 0
+        hours: 0,
+        isOpen: false
     }
 
-    handleClick = (e) => {
+    openCloseTable = () => {
         this.setState({
-            hours: this.state.hours+1
+            isOpen : !this.state.isOpen
         })
     }
     
     render() {
         const { user } = this.props;
-        console.log(user);
-
+        
+        const events1 = ["volunteer1", "volunteer2", "volunteer3"]
         // Render user data into a table
         // Table header: First, Last, isAdmin, etc.
         // Table data: Aditya, Dave, True, etc.
 
         return (
             <>
-                <div className="hello">This is a table</div>
-                <p>I have {this.state.hours} hours in BMES!</p>
-                <button onClick={this.handleClick}>Add Hour</button>
-                <div> { !user.isAdmin && 'I am not an admin' } </div>
-            </>
-        )
-    }
-}
+                {user ? (
+                    <>
+                        <div className="block">
+                            <Sidenav user={user}/>
+                        </div>
 
+                        <div className="allData">
+                            <h1 className="blue-text">User Data:</h1>
+
+                            <button onClick={this.openCloseTable} className="blue btn waves-effect waves-light openButton">
+                                Click Here to Open/Close the Table!
+                            </button>
+
+                            <table className="centered">
+                                <tr className="r1">
+                                    <th>First Name</th>
+                                    <th>Last Name</th>
+                                </tr>
+                                {this.state.isOpen && (
+                                    <tr className="r2">
+                                        <td>{user.first}</td>
+                                        <td>{user.last}</td>
+                                    </tr>
+                                )}   
+                            </table>
+
+                            <table className="centered">
+                                <tr className="r3">
+                                    <th>Volunteer Hours</th>
+                                    <th>Net ID</th>
+                                </tr>
+                                {this.state.isOpen && (
+                                    <tr className="r4">
+                                        <td>{user.hours}</td>
+                                        <td>{user.netid}</td>
+                                    </tr>
+                                )}
+                            </table>
+
+                            <h4 className="blue-text">Past Events:</h4>
+                    
+                            {events1 && (
+                                <ul>
+                                    {events1.map(event => (
+                                        <li>{event}</li>
+                                    ))}
+                                </ul>
+                            )}
+                        </div>
+                
+                    </> ) : <Loader/> 
+                }
+            </>
+        );
+    };
+};
+
+ 
 const mapStateToProps = (state) => ({
     auth: state.auth,
     user: state.user,
     error: state.error
   });
+  
+  export default connect(mapStateToProps)(Stats);
 
-export default connect(mapStateToProps)(Stats);
